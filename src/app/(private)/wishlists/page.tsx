500efc48da94516e0a0bd3a4fdbe1f73467a569c
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-05 21:45:40 +0000

    feat: Add grid basic structure for desktop

--- src/app/(private)/wishlists/page.tsx
+++ src/app/(private)/wishlists/page.tsx
@@ -1,15 +1,16 @@
-import React , {FC} from "react"
+import React, { FC } from "react";
 import Link from "next/link";
 
-const Wishlists:FC = () => {
+const Wishlists: FC = () => {
   return (
-    <div>
-      Wishlists page connected
-      <div className="">
-        <Link href="/">Home</Link>
-      </div>
+    <div className=" w-screen h-screen bg-blue-800 grid grid-cols-12 grid-rows-12">
+      <div className="bg-red-500 col-span-12 row-span-1">Navbar</div>
+      <div className="bg-green-500 col-span-2 row-span-1">Sidebar navbar</div>
+      <div className="bg-yellow-500 col-span-10 row-span-1">Content navbar</div>
+      <div className="bg-orange-500 col-span-2 row-span-10">Sidebar Content</div>
+      <div className="bg-purple-500 col-span-10 row-span-10">Content</div>
     </div>
-  )
+  );
 };
 
 export default Wishlists;
