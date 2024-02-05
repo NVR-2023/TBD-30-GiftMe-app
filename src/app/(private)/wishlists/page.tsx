30047095f14133c7d1a68624430ad4ae59fcd207
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-05 22:15:35 +0000

    feat: establish x and y gaps in the basic desktop layout

--- src/app/(private)/wishlists/page.tsx
+++ src/app/(private)/wishlists/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 const Wishlists: FC = () => {
   return (
-    <div className=" w-screen h-screen bg-blue-800 grid grid-cols-12 grid-rows-12">
-      <div className="bg-red-500 col-span-12 row-span-1">Navbar</div>
-      <div className="bg-green-500 col-span-2 row-span-1">Sidebar navbar</div>
-      <div className="bg-yellow-500 col-span-10 row-span-1">Content navbar</div>
-      <div className="bg-orange-500 col-span-2 row-span-10">Sidebar Content</div>
-      <div className="bg-purple-500 col-span-10 row-span-10">Content</div>
+    <div className=" w-screen h-screen bg-blue-800">
+      <div className=" h-full w-full py-3 px-3 grid grid-cols-12 grid-rows-12 gap-x-1 gap-y-1">
+        <div className="bg-red-500 col-span-12 row-span-1">Navbar</div>
+        <div className="bg-green-500 col-span-2 row-span-1">Sidebar navbar</div>
+        <div className="bg-yellow-500 col-span-10 row-span-1">Content navbar</div>
+        <div className="bg-orange-500 col-span-2 row-span-10">Sidebar Content</div>
+        <div className="bg-purple-500 col-span-10 row-span-10">Content</div>
+      </div>
     </div>
   );
 };
