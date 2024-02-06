ffca2950c42623389c017f0e6579b8feb983b578
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-06 20:54:14 +0000

    fix: prefix whishlists with use client to be able to sue context

--- src/app/(private)/wishlists/page.tsx
+++ src/app/(private)/wishlists/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import React, { FC } from "react";
 import Navbar from "@/sections/navbar/navbar";
 import ContentNavbar from "./sub-components/content-navbar";
 import AsideNavbar from "./sub-components/aside-navbar";
 import Link from "next/link";
 
+import { useDarkModeContext } from "@/contexts/darkmode-context";
+
 const Wishlists: FC = () => {
-  return (
+const { darkMode , setDarkMode } = useDarkModeContext();
+
+return (
     <div className=" w-screen h-screen bg-blue-800">
       <div className=" h-full w-full py-3 px-3 grid grid-cols-12 grid-rows-10 gap-x-1 gap-y-1">
         <header className="bg-red-500 col-span-12 row-span-1"><Navbar/></header>
